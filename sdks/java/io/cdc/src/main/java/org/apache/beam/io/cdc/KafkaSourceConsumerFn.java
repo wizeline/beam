522afb4ccb566838088e66233a0327dffe25b164
Carlos Domínguez <carlos.dominguez@wizeline.com>
2020-12-16 12:24:45 -0600

    Adding new restriction
    Uses a time-based restriction

--- sdks/java/io/cdc/src/main/java/org/apache/beam/io/cdc/KafkaSourceConsumerFn.java
+++ sdks/java/io/cdc/src/main/java/org/apache/beam/io/cdc/KafkaSourceConsumerFn.java
@@ -24,6 +24,7 @@ import org.apache.beam.sdk.transforms.splittabledofn.RestrictionTracker;
 import org.apache.kafka.connect.source.SourceConnector;
 import org.apache.kafka.connect.source.SourceRecord;
 import org.apache.kafka.connect.source.SourceTask;
+import org.joda.time.DateTime;
 import org.joda.time.Duration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -42,6 +43,9 @@ public class KafkaSourceConsumerFn<T> extends DoFn<Map<String, String>, T> {
   private static final Logger LOG = LoggerFactory.getLogger(KafkaSourceConsumerFn.class);
   public static final String BEAM_INSTANCE_PROPERTY = "beam.parent.instance";
 
+  public static long minutesToRun = 2; // Pass this as parameter ??
+  public static DateTime startTime;
+
   private final Class<? extends SourceConnector> connectorClass;
   private final SourceRecordMapper<T> fn;
   protected static final Map<String, RestrictionTracker<DebeziumOffsetHolder,  Map<String, Object>>>
@@ -54,6 +58,8 @@ public class KafkaSourceConsumerFn<T> extends DoFn<Map<String, String>, T> {
 
   @GetInitialRestriction
   public DebeziumOffsetHolder getInitialRestriction(@Element Map<String, String> unused) throws IOException {
+    // First run
+    KafkaSourceConsumerFn.startTime = new DateTime();
     return new DebeziumOffsetHolder(null, null);
   }
 
