1cf8d2044e3c6497d2ff1e183bb18ad43ad979f9
Carlos Domínguez <carlos.dominguez@wizeline.com>
2020-12-29 15:41:28 -0600

    Added Basic Connector doc

--- sdks/java/io/cdc/src/main/java/org/apache/beam/io/cdc/BasicConnector.java
+++ sdks/java/io/cdc/src/main/java/org/apache/beam/io/cdc/BasicConnector.java
@@ -22,6 +22,9 @@ import org.apache.kafka.connect.source.SourceConnector;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Base class for a complex connector
+ */
 public class BasicConnector {
     private Class<?> connectorClass;
     private String username;
@@ -32,58 +35,114 @@ public class BasicConnector {
     private SourceConnector connector;
     private Map<String, String> configuration;
 
+    /**
+     * Connector Class getter
+     * @return connectorClass to be used (MySQL, PostgreSQL or SQLServer)
+     */
     public Class<?> getConnectorClass() {
         return connectorClass;
     }
 
+    /**
+     * Connector Class setter
+     * @param connectorClass must be a supported ConnectorClass {@link io.debezium.connector}
+     */
     public void setConnectorClass(Class<?> connectorClass) {
         this.connectorClass = connectorClass;
     }
 
+    /**
+     * username getter
+     * @return Database username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * username setter
+     * @param username Database username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * password getter
+     * @return Database password
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * password setter
+     * @param password Database password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * host getter
+     * @return Database host
+     */
     public String getHost() {
         return host;
     }
 
+    /**
+     * host setter
+     * @param host Database host
+     */
     public void setHost(String host) {
         this.host = host;
     }
 
+    /**
+     * port getter
+     * @return Database port
+     */
     public String getPort() {
         return port;
     }
 
+    /**
+     * port setter
+     * @param port Database port
+     */
     public void setPort(String port) {
         this.port = port;
     }
 
+    /**
+     * SourceConnector setter
+     * @param connector SourceConnector
+     */
     public void setConnector(SourceConnector connector) {
         this.connector = connector;
     }
 
+    /**
+     * Connection Properties getter
+     * @return Key - Value Connection properties to be used.
+     */
     public Map<String,String> getConnectionProperties() {
         return connectionProperties;
     }
 
+    /**
+     * Connection Properties setter
+     * @param connectionProperties Key - Value Connection properties to be used.
+     */
     public void setConnectionProperties(Map<String,String> connectionProperties) {
         this.connectionProperties = connectionProperties;
     }
 
+    /**
+     * Configuration getter
+     * @return Key - Value configuration to be used.
+     */
     public Map<String, String> getConfiguration() {
         if(this.configuration != null) {
             return this.configuration;
@@ -104,6 +163,10 @@ public class BasicConnector {
         return configuration;
     }
 
+    /**
+     * SourceConnector getter
+     * @return SourceConnector to be used
+     */
     public SourceConnector getConnector() {
         return connector;
     }
