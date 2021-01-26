7a3bc42fd8feffb9e4ed52dde4f240e7753a62ba
juanitodread <juanitodread@gmail.com>
2021-01-26 11:46:11 -0800

    Remove DB2 code

--- sdks/java/io/cdc/src/test/java/org/apache/beam/io/cdc/DebeziumIOConnectorTest.java
+++ sdks/java/io/cdc/src/test/java/org/apache/beam/io/cdc/DebeziumIOConnectorTest.java
@@ -17,11 +17,8 @@
  */
 package org.apache.beam.io.cdc;
 
-import io.debezium.connector.db2.Db2Connector;
 import io.debezium.connector.postgresql.PostgresConnector;
 import io.debezium.connector.sqlserver.SqlServerConnector;
-
-import io.debezium.relational.history.FileDatabaseHistory;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.coders.StringUtf8Coder;
 import org.apache.beam.sdk.options.PipelineOptions;
@@ -121,32 +118,4 @@ public class DebeziumIOConnectorTest {
 
         p.run().waitUntilFinish();
     }
-    
-    @Test
-    public void testDebeziumIOdb2() {
-        PipelineOptions options = PipelineOptionsFactory.create();
-        Pipeline p = Pipeline.create(options);
-        p.apply(DebeziumIO.readAsJson().
-                        withConnectorConfiguration(
-                                DebeziumIO.ConnectorConfiguration.create()
-                                        .withUsername("db2inst1")
-                                        .withPassword("=Password!")
-                                        .withConnectorClass(Db2Connector.class)
-                                        .withHostName("127.0.0.1")
-                                        .withPort("50000")
-                                        .withConnectionProperty("database.dbname", "TESTDB")
-                                        .withConnectionProperty("tasks.max", "1")
-                                        .withConnectionProperty("database.hostname","db2server")
-                                        .withConnectionProperty("database.server.name", "db2server")
-                                        .withConnectionProperty("database.cdcschema", "ASNCDC")
-                                        .withConnectionProperty("database.include.list", "TESTDB")
-                                        .withConnectionProperty("table.include.list", "DB2INST1.CUSTOMERS")
-                                        .withConnectionProperty("database.history", FileDatabaseHistory.class.getName())
-                                        .withConnectionProperty("database.history.file.filename", "file2-history.dat")
-                        )
-        ).setCoder(StringUtf8Coder.of());
-
-        p.run().waitUntilFinish();
-    }
-
 }
