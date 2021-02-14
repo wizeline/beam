93eec0d9afc28b532eba08f36e4bc2bcdf9025f5
juanitodread <juanitodread@gmail.com>
2021-02-13 21:56:06 -0800

    Rename connector package cdc=>debezium. Update doc references Fix code style on DebeziumIOMySqlConnectorIT

--- sdks/java/io/debezium/src/test/java/org/apache/beam/io/debezium/DebeziumIOTest.java
+++ sdks/java/io/debezium/src/test/java/org/apache/beam/io/debezium/DebeziumIOTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.io.cdc;
+package org.apache.beam.io.debezium;
 
 import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
@@ -25,7 +25,7 @@ import io.debezium.connector.mysql.MySqlConnector;
 import io.debezium.connector.mysql.MySqlConnectorConfig;
 import java.io.Serializable;
 import java.util.Map;
-import org.apache.beam.io.cdc.DebeziumIO.ConnectorConfiguration;
+import org.apache.beam.io.debezium.DebeziumIO.ConnectorConfiguration;
 import org.apache.kafka.common.config.ConfigValue;
 import org.junit.Test;
 import org.junit.runner.RunWith;
--- sdks/java/io/debezium/src/test/java/org/apache/beam/io/debezium/OffsetTrackerTest.java
+++ sdks/java/io/debezium/src/test/java/org/apache/beam/io/debezium/OffsetTrackerTest.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.beam.io.cdc;
+package org.apache.beam.io.debezium;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
